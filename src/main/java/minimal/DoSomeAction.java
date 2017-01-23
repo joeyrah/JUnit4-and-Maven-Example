0e194fee25e37f6cb1ca62ffd0202f52804b7799
joeyrah <joey.rah@gmail.com>
2017-01-23 11:29:57 -0800

    add

--- src/main/java/minimal/DoSomeAction.java
+++ src/main/java/minimal/DoSomeAction.java
@@ -6,4 +6,9 @@ package minimal;
  * @author Sascha Tayefeh
  */
 public class DoSomeAction {
+  
+  public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
 }
